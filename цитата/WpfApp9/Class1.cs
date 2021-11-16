9bc1e9d0a12e89a8e9e36f89a4ca4efc794ce758
grigoreva08evgenia <72446984+grigoreva08evgenia@users.noreply.github.com>
2021-11-16 10:09:05 +0900

    Add files via upload

---
+++ цитата/WpfApp9/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp9
+{
+    class Class1
+    {
+        public int idtable1 { get; set; }
+
+        public string quoteText { get; set; }
+
+        public string quoteAuthor { get; set; }
+
+        public string quoteLink { get; set; }
+    }
+}
---
+++ цитата/WpfApp9/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace WpfApp9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Class2 db = new Class2();
+        public MainWindow()
+        {
+            InitializeComponent();
+            db.CreateStrConnection();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnGetQuate_Click(object sender, RoutedEventArgs e)
+        {
+           
+            Quote qoute = new Quote();
+
+            qoute = qoute.GetQuote();
+
+            lblQuate.Text = qoute.quoteText;
+            lblAutor.Text = qoute.quoteAuthor;
+            lblLink.Text = qoute.quoteLink;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            db.Addquote(lblQuate.Text, lblAutor.Text, lblLink.Text);
+            MessageBox.Show("Добавлена в избранное");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Window1 taskwindow = new Window1();
+            taskwindow.Show();
+            this.Hide();
+        }
+    }
+}
